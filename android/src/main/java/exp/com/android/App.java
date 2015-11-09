2c527bcbaacbc2814e0c33eb22736779cf6e3418
Cesar Oyarzun <cesar.oyarzun@scala.com>
2015-11-09 15:19:29 -0500

    first commit

---
+++ android/src/main/java/exp/com/android/App.java
@@ -0,0 +1,13 @@
+package exp.com.android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
